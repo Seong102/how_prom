5deca7a0e72cab700b70f1b449f2bae9a14e5dff
kimeunbinbev <hesed825@gmail.com>
2026-05-20 17:08:16 +0900

    문제 등록 페이지 프론트엔드 작업 및 백엔드 작업 완료(임시)

---
+++ src/main/java/com/howprom/common/entity/User.java
@@ -0,0 +1,17 @@
+package com.howprom.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email; // 내가 테스트할 때 필요한 최소 필드만 작성
+}
